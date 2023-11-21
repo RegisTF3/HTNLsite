6391f1b78c892c854b6c13a834e68dce53c04bf6
RegisTF3 <182210131@senacinformatica.br>
2023-11-21 12:08:34 -0300

    começando Java

---
+++ Livr.js
@@ -0,0 +1,43 @@
+let conta = 0;
+var Livross = array();
+Livross [0] = "image/liv1.png";
+Livross [1] = "image/liv2.png";
+Livross [2] = "image/liv3.png";
+Livross [3] = "image/liv4.png";
+Livross [4] = "image/livr1.png";
+Livross [5] = "image/livr2.png";
+Livross [6] = "image/livr3.png";
+Livross [7] = "image/livr4.png";
+Livross [8] = "image/livro5.png";
+Livross [9] = "image/livro6.png";
+Livross [10] = "image/livro7.png";
+Livross [11] = "image/livro8.png";
+Livross [12] = "image/livro9.png";
+Livross [13] = "image/livro10.png";
+Livross [14] = "image/livro11.png";
+Livross [15] = "image/livro12.png";
+Livross [16] = "image/livro14.png";
+Livross [17] = "image/livro15.png";
+Livross [18] = "image/livro16.png";
+Livross [19] = "image/livro17.png";
+Livross [20] = "image/livro18.png";
+Livross [21] = "image/livro19.png";
+Livross [22] = "image/livro20.png";
+Livross [23] = "image/Livro21.png";
+Livross [24] = "image/livro22.png";
+Livross [25] = "image/livro23.png";
+Livross [26] = "image/livro24.png";
+Livross [27] = "image/livro25.png";
+Livross [28] = "image/liv2.png";
+Livross [29] = "image/liv2.png";
+Livross [30] = "image/liv2.png";
+Livross [31] = "image/liv2.png";
+
+
+const btEsq = document.getElementById('esquerdo');
+btEsq.addEventListener("click",proximaImagen);
+const l1 = document.getElementById('l1');
+function proximaImagen () {
+   l1.src="image/livro14.png";
+   
+}
