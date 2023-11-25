fde5f74249caba32a94264648e73900887f34f45
RegisTF3 <regis.tfonseca3@gmail.com>
2023-11-25 08:28:47 -0300

    duas paginas prontas acho

--- Livr.js
+++ Livr.js
@@ -1,4 +1,4 @@
-let conta = 0;
+/*let conta = 0;
 var Livross = array();
 Livross [0] = "image/liv1.png";
 Livross [1] = "image/liv2.png";
@@ -28,16 +28,72 @@ Livross [24] = "image/livro22.png";
 Livross [25] = "image/livro23.png";
 Livross [26] = "image/livro24.png";
 Livross [27] = "image/livro25.png";
-Livross [28] = "image/liv2.png";
-Livross [29] = "image/liv2.png";
-Livross [30] = "image/liv2.png";
-Livross [31] = "image/liv2.png";
+Livross [28] = "image/livro26.png";
+Livross [29] = "image/livro27.png";
+Livross [30] = "image/livro28.png";
+Livross [31] = "image/livro29.png";*/
 
 
+const btDir = document.getElementById('direita');
+btDir.addEventListener("click",proximaImagen);
 const btEsq = document.getElementById('esquerdo');
-btEsq.addEventListener("click",proximaImagen);
+btEsq.addEventListener("click",voltarImagem);
 const l1 = document.getElementById('l1');
+const l2 = document.getElementById('l2');
+const l3 = document.getElementById('l3');
+const l4 = document.getElementById('l4');
+const l5 = document.getElementById('l5');
+const l6 = document.getElementById('l6');
+const l7 = document.getElementById('l7');
+const l8 = document.getElementById('l8');
+const l9 = document.getElementById('l9');
+const l10 = document.getElementById('l10');
+const l11 = document.getElementById('l11');
+const l12 = document.getElementById('l12');
+const l13 = document.getElementById('l13');
+const l14 = document.getElementById('l14');
+const l15 = document.getElementById('l15');
+const l16 = document.getElementById('l16');
+
 function proximaImagen () {
    l1.src="image/livro14.png";
-   
-}
+   l2.src="image/livro15.png";
+   l3.src="image/livro16.png";
+   l4.src="image/livro17.png";
+
+   l5.src="image/livro18.png";
+   l6.src="image/livro19.png";
+   l7.src="image/livro20.png";
+   l8.src="image/Livro21.png";
+
+   l9.src="image/livro22.png";
+   l10.src="image/livro23.png";
+   l11.src="image/livro24.png";
+   l12.src="image/livro25.png";
+
+   l13.src="image/livro26.png";
+   l14.src="image/livro27.png";
+   l15.src="image/livro28.png";
+   l16.src="image/livro29.png";
+   }
+   function voltarImagem (){
+    l1.src="image/liv1.png";
+    l2.src="image/liv2.png";
+    l3.src="image/liv3.png";
+    l4.src="image/liv4.png";
+
+    l5.src="image/livr1.png";
+    l6.src="image/livr2.png";
+    l7.src="image/livr3.png";
+    l8.src="image/livr4.png";
+
+    l9.src="image/livro5.png";
+    l10.src="image/livro6.png";
+    l11.src="image/livro7.png";
+    l12.src="image/livro8.png";
+
+    l13.src="image/livro9.png";
+    l14.src="image/livro10.png";
+    l15.src="image/livro11.png";
+    l16.src="image/livro12.png";
+   }
